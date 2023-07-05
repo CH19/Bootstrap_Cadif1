bf117d454d0eb6322b4920c03735b4173beae498
christian <chrislacruz2005@gmail.com>
2023-07-05 17:22:21 -0400

    change leftbar notification, add accordedon in the leftbar using Javascript

--- challenge-1-1/script.js
+++ challenge-1-1/script.js
@@ -1,3 +1,35 @@
 'use strict'
-
+let estrucutra = ``;
+const Lista = {
+    Nombre: ['Friends','Groups', 'MarketPlace', 'Watch', 'Remembers', 'Saved', 'Pages', 'Events', 'Works', 'See More'],
+    Logo: ['<ion-icon name="people-outline"></ion-icon>','<ion-icon name="person-circle-outline"></ion-icon>','<ion-icon name="storefront-outline"></ion-icon>', '<ion-icon name="play-circle-outline"></ion-icon>','<ion-icon name="time-outline"></ion-icon>','<ion-icon name="bookmark-outline"></ion-icon>','<ion-icon name="flag-outline"></ion-icon>','                                       <ion-icon name="calendar-outline"></ion-icon>','<ion-icon name="bag-handle-outline"></ion-icon>','                                       <ion-icon name="chevron-down-circle-outline"></ion-icon>' ],
+    id: []
+}
+ console.log(Lista.Logo[0] + ',' + Lista.Nombre[0]);
+console.log(estrucutra)
+const menusito = document.querySelector('#Menusito');
+Lista.Nombre.forEach((Name, index) =>{
+    let idTemporal = (Math.random() * 100).toString(36).slice(3)
+    Lista.id.push(idTemporal)
+    let structure = `</li>
+    <div id="${Lista.id[index]}" class="d-flex flex-row accordion">
+                  <div class="accordion-item">
+                    <div class="accordion-header">
+                      <button class="accordion-button text-dark d-flex align-items-center justify-content-center" type="button" data-bs-toggle="collapse" data-bs-target="#${Lista.id[index]}" aria-expanded="false" aria-controls="${Lista.id[index]}">                                         
+                      <figure class="mx-4 text-primary">${Lista.Logo[index]}</figure>
+                       ${Name}</button>
+                    </div>
+                    <div id="${Lista.id[index]}" class="accordion-collapse collapse" style="width: 300px" data-bs-parent="#${Lista.id[index]}">
+                     <div class="accordion-body">
+                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     </div>
+                   </div>
+                   </div>
+    </div>
+  <li>
+`
+estrucutra += structure;
+})
+console.log(estrucutra)
+$('#Menusito').append(estrucutra);
 
